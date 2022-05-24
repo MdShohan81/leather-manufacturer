8ea1e8ae7ac0bf86b4053f9dabb4d02880fbb474
MdShohan81 <mdshohan6563@gmail.com>
2022-05-24 09:16:50 +0600

    added require auth for private route

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/Home/Home/Home';
 import Login from './pages/Login/Login/Login';
 import Register from './pages/Login/Register/Register';
+import RequireAuth from './pages/Login/RequireAuth/RequireAuth';
 import ProductDetail from './pages/ProductDetail/ProductDetail';
 
 
@@ -13,8 +14,13 @@ function App() {
         <Header></Header>
         <Routes>
             <Route path='/' element={<Home></Home>}></Route>
-            <Route path='/product/:productId' element={<ProductDetail>5</ProductDetail>}></Route>
-            <Route path='/login' element={<Login></Login>}></Route>>
+            
+            <Route path='/product/:productId' element={
+            <RequireAuth>
+              <ProductDetail>5</ProductDetail>
+            </RequireAuth>
+          }></Route>
+            <Route path='/login' element={<Login></Login>}></Route>
             <Route path='/register' element={<Register></Register>}></Route>
         </Routes>
     </div>
